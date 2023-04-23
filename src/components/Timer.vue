5772d03e090786b229c1974651d2a6864f519c6b
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-04-24 00:32:31 +0900

    add: timer component

---
+++ src/components/Timer.vue
@@ -0,0 +1,29 @@
+<!-- 秒を渡して特定のフォーマットにする -->
+<template>
+    <span class="text-h1 time">
+        {{`${minString}:${secString}`}}
+    </span>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { Ref } from 'vue';
+import { reactive } from 'vue';
+
+type Props = {
+    /**
+     * 秒数
+     */
+    sec: number
+}
+
+const props = defineProps<Props>()
+
+const minString: Ref<string> = ref(String(Math.trunc(props.sec / 60)).padStart(2, "0"))
+const secString: Ref<string> = ref(String(props.sec % 60).padStart(2, "0"))
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
-  <HelloWorld />
+  <div>
+    <Timer :sec="100" />
+  </div>
 </template>
 
 <script lang="ts" setup>
+  import Timer from '@/components/Timer.vue'
   import HelloWorld from '@/components/HelloWorld.vue'
 </script>
