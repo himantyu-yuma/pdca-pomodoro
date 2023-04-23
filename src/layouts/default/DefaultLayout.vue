914d52c015e3981625922fc9fc61f1c67475e5f1
himantyu-yuma <lipton.happy.milktea@gmail.com>
2023-04-24 00:31:22 +0900

    rename

---
+++ src/layouts/default/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <app-bar />
+
+    <default-page />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import AppBar from './AppBar.vue'
+import DefaultPage from './DefaultPage.vue'
+</script>
